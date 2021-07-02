938ef77786e4bc715a5db34d86eedf77a0e1b301
herokutests <85432009+herokutests@users.noreply.github.com>
2021-07-02 15:02:34 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
-import github
+
 
 app = Flask(__name__)
 api = Api(app)
 
-g = github.Github("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")
-repo = g.get_repo("herokutests/provideip")
 
 
 class Ip(Resource):
@@ -19,8 +17,6 @@ class Update(Resource):
     def put(self, newip):
         with open("ip.txt", "w") as text:
             text.write(newip)
-        contents = repo.get_contents("/ip.txt")
-        repo.update_file("ip.txt", "file_updated_automatically", newip, contents.sha)
         return "Updated", 201
 
 
